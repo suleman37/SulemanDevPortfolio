e4f765802dabd8696f9a4d47d7a45a739fa6be63
DESKTOP-2UVV9SF\Dream Monster <sulemanshakeel37@gmail.com>
2024-10-19 14:14:12 -0700

    Pushing Code Portfolio

---
+++ src/components/Banner.js
@@ -0,0 +1,113 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState("");
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = ["Frontend Developer"];
+  const period = 2000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta((prevDelta) => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex((prevIndex) => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === "") {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 1);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex((prevIndex) => prevIndex + 1);
+    }
+  };
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__fadeIn" : ""
+                  }
+                >
+                  <span className="tagline">Welcome to my Portfolio</span>
+                  <h1>
+                    {`Hi! I'm Suleman`}{" "}
+                    <span
+                      className="txt-rotate"
+                      dataPeriod="1000"
+                      // data-rotate='["Web Developer"]'
+                    >
+                      <span className="wrap">{text}</span>
+                    </span>
+                  </h1>
+                  <p>
+                    As a front-end developer, I specialize in creating dynamic
+                    and user-friendly web applications using technologies such
+                    as WordPress , JavaScript, React.js, and HTML/CSS. With over
+                    a year of experience in web development, I have a strong
+                    understanding of the front-end development process and best
+                    practices. I am passionate about crafting high-quality user
+                    experiences and paying attention to detail in my work. My
+                    proactive approach and dedication to continuous learning
+                    enable me to stay current with industry trends and deliver
+                    exceptional results in every project I undertake.
+                  </p>
+                  <button onClick={() => console.log("connect")}>
+                    Let’s Connect <ArrowRightCircle size={25} />
+                  </button>
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <TrackVisibility>
+              {({ isVisible }) => (
+                <div
+                  className={
+                    isVisible ? "animate__animated animate__zoomIn" : ""
+                  }
+                >
+                  <img src={headerImg} alt="Header Img" />
+                </div>
+              )}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
---
+++ src/components/Projects.js
@@ -0,0 +1,122 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/aq.jfif";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import grill from "../assets/img/grill.jfif";
+import zohan from "../assets/img/zohan.jfif";
+import naveen from "../assets/img/e-comm.jfif";
+import portal from "../assets/img/portal.jfif";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects1 = [
+    {
+      title: "Attendence Portal",
+      description: "Frontend Development",
+      imgUrl: portal,
+    },
+  ];
+  const projects2 = [
+    {
+      title: "Resturent Website",
+      description: "Frontend Development",
+      imgUrl: grill,
+    },
+    {
+      title: "Digital Marketing Website",
+      description: "Frontend Development",
+      imgUrl: zohan,
+    }
+    ,
+    {
+      title: "E-Commerce Website",
+      description: "Frontend Development",
+      imgUrl: naveen,
+    }
+  ];
+  const projects3 = [
+    {
+      title: "Bussiness Website",
+      description: "Frontend Development",
+      imgUrl: projImg1,
+    }
+  ];
+
+  return (
+    <section className="project" id="projects">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Throughout my web development career, I have been fortunate to work on a number of exciting and challenging projects, including [E-Commerce Websites, Bussiness Websites and System Management Softwares etc ]. These projects have allowed me to demonstrate my expertise in development technologies and have given me the opportunity to take on various roles. I am proud of the results I have achieved on these projects and am always looking for new opportunities to apply my skills and experience to make a positive impact.</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Software Frontend</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Frontend Website</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Business Websites</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                    <Row>
+                        {
+                          projects1.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                    <Row>
+                        {
+                          projects2.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                    <Row>
+                        {
+                          projects3.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2} alt=""/>
+    </section>
+  )
+}
