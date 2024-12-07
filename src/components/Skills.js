79533cba8580af67856fc08f47ab631f7c3e4fa0
suleman37 <sulemanshakeel37@gmail.com>
2024-12-07 20:34:15 +0500

    fixing errors

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,79 +1,86 @@
 import meter1 from "../assets/img/meter1.svg";
 import meter2 from "../assets/img/meter2.svg";
 import meter3 from "../assets/img/meter3.svg";
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import arrow1 from "../assets/img/arrow1.svg";
-import arrow2 from "../assets/img/arrow2.svg";
-import colorSharp from "../assets/img/color-sharp.png"
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import colorSharp from "../assets/img/color-sharp.png";
 
 export const Skills = () => {
   const responsive = {
     superLargeDesktop: {
-      // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5
+      items: 5,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 3
+      items: 3,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2
+      items: 2,
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
-      items: 1
-    }
+      items: 1,
+    },
   };
 
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
-                        <h2>Skills</h2>
-                        <p>I have extensive experience with these technologies, having worked on numerous projects utilizing these tools.<br /> I am confident in my ability to apply these skills to a variety of projects and environments.</p>
-                        <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                        <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>HTML/CSS</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>JS/ES6</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>React-Bootstrap</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Tailwind CSS</h5>
-                            </div> <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Material UI</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>React Js</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Next Js</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>WordPress</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
+              <h2>Skills</h2>
+              <p>
+                I have extensive experience with these technologies, having
+                worked on numerous projects utilizing these tools.
+                <br /> I am confident in my ability to apply these skills to a
+                variety of projects and environments.
+              </p>
+              <Carousel
+                responsive={responsive}
+                infinite={true}
+                className="skill-slider" // Removed 'owl-carousel' and 'owl-theme'
+              >
+                <div className="item">
+                  <img src={meter1} alt="HTML/CSS" />
+                  <h5>HTML/CSS</h5>
                 </div>
+                <div className="item">
+                  <img src={meter3} alt="JS/ES6" />
+                  <h5>JS/ES6</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="React-Bootstrap" />
+                  <h5>React-Bootstrap</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Tailwind CSS" />
+                  <h5>Tailwind CSS</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="Material UI" />
+                  <h5>Material UI</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="React Js" />
+                  <h5>React Js</h5>
+                </div>
+                <div className="item">
+                  <img src={meter2} alt="Next Js" />
+                  <h5>Next Js</h5>
+                </div>
+                <div className="item">
+                  <img src={meter1} alt="WordPress" />
+                  <h5>WordPress</h5>
+                </div>
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Background" />
     </section>
-  )
-}
+  );
+};
